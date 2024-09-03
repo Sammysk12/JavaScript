bfd3da545a42a9040a07f36e4932437a50665251
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-03 13:42:41 +0530

    Learned Datatypes and variables

---
+++ first.js
@@ -0,0 +1,28 @@
+console.log("Apna College");
+let fullName = "Tony Stark";
+var foo =323;  //inappropriate way to declare
+let age = 22;
+age= 24; //updated value of age is 24
+rad = 14.56;
+price = 99.99;
+console.log(fullName);
+
+isFollow = false;
+
+
+const studentData = {
+    fullnName : "Rahul Kumar",
+    age: 20,
+    cgpa:8.2,
+    isPass: true
+
+};
+
+console.log(studentData)
+
+studentData[fullName] ="Rahul Sharma";
+studentData.age = studentData["age"]+1;
+
+console.log(studentData);
+
+
