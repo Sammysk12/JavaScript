705d8df0c3e6cd9ac234eb6c7a461f166d326c60
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-05 13:07:30 +0530

    Solved practice questions on loops

---
+++ Practice_Set_3/script.js
@@ -0,0 +1,28 @@
+
+//Practice -1  Print even upto 100
+
+// let num = 100;
+
+// let i = 0;
+// while(i<=num){
+//     if(i%2===0){
+//         console.log(i)
+//         i++;
+//     }
+// }
+
+
+// Guess the number
+
+let gameNum = 25;
+
+let userNum = prompt("Guess the number: "); 
+
+while(gameNum != userNum){
+   userNum = prompt("Wrong !Guess the number: ");
+
+
+}
+
+alert("Yess !")
+
