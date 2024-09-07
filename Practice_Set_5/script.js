1fb1f42769e0fb931adbf6755b38046d2ae1de79
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-08 00:22:10 +0530

    Solved more questions about array methods

--- Practice_Set_5/script.js
+++ Practice_Set_5/script.js
@@ -32,3 +32,20 @@ for(let val of items){
     console.log(`The final price with 10% discount is ${val}`);
 
 }
+
+
+//Q3
+
+let companies =["Bloomberg", "Microsoft", "Uber", "Google","IBM", "Netflix"]
+console.log(companies);
+
+
+companies.shift();
+console.log(companies);
+
+companies.splice(1,1,"Ola")
+console.log(companies)
+
+
+companies.push("Amazon")
+console.log(companies)
