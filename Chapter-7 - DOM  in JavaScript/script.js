531803d76c786a7ebd21f8145fa04034d5bbd113
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-13 20:18:37 +0530

    Leanred about the DOM- Document Object Model in the JavaScript

---
+++ Chapter-7 - DOM  in JavaScript/script.js
@@ -0,0 +1,45 @@
+// function handleSubmit(){
+//     alert("Hi!")
+// }
+
+
+// //get element by ID
+// let heading = document.getElementById("heading");
+// // console.log(heading);
+// // console.dir(heading);
+
+// //get element by className
+// let subHeading = document.getElementsByClassName("sub-heading");
+
+// console.log(subHeading);
+// console.dir(subHeading);
+
+
+// //get elements by tag --> HTML tag
+
+// let para = document.getElementsByTagName("p");
+
+// console.dir(para);
+
+
+// //Another powerful way for DOM manipulation is
+// //Query Selector
+// //It detects the which tpye of element is given id/className/tag
+
+// //returns first element which it founds in HTML
+// let p = document.querySelector("p");
+// console.dir(p);
+
+// //Returns all elements value which it found in the HTML
+// // let head = document.querySelectorAll(".sub-heading");
+
+// // console.dir(head);
+
+// let head1 = document.querySelectorAll(".first");
+
+// console.dir(head1);
+
+
+let div = document.querySelector("div");
+
+console.dir(div);
