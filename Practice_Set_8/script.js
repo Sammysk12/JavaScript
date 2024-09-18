69aae04dbb2fab56119f41877b4ac64802334cc7
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-18 12:24:50 +0530

    Solved practice set about the events in JavaScript

--- Practice_Set_8/script.js
+++ Practice_Set_8/script.js
@@ -1,48 +1,13 @@
-//Q1
-//Hello JavaScript 
-//add  "from apna college"
+//toggling = changing between the 1st state and 2nd state
 
+modeBtn = document.querySelector("#btn1");
 
-let change = document.querySelector("h2");
+modeBtn.addEventListener("click", () =>{
+    if(document.querySelector("body").style.backgroundColor = "#fff"){
+        document.querySelector("body").style.backgroundColor = "#1c1c1c";
+    }else{
+        document.querySelector("body").style.backgroundColor = "#fff";
+    }
+    
 
-console.dir(change.innerText);
-
-change.innerText = change.innerText + " from apna college";
-
-
-
-
-// Q2
-// Add 3 divs and change its value by selection proper value
-let div = document.querySelectorAll(".box");
-console.dir(div);
-
-div[0].innerText= "New unique val 1";
-div[1].innerText= "New unique val 2";
-div[2].innerText= "New unique val 3";
-
-
-let i = 1
-for(let val of div){
-    val.innerText = `New val ${i}`;
-    i++;
-}
-
-//Q3
-//Add button in div at start without touchinh HTMl and give bg color red and txt color white
-let btn1 = document.createElement("button");
-btn1.innerText = "Click me!"
-btn1.style.backgroundColor = "red"
-btn1.style.color = "white";
-
-document.querySelector("body").prepend(btn1);
-
-
-// Q2
-// Add a para and change styling by changing its class name
-let para = document.querySelector(".fir");
-// para.setAttribute("class", "sec");
-para.classList.add("sec")
-
-
-
+})
